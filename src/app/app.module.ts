4d485a00656819330c47582b33dbb7692852311b
AdrielleS <adrielle.tatiane@hotmail.com>
2018-04-25 12:09:53 -0300

    projeto ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,45 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule} from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SigninPage} from '../pages/signin/signin';
+import {RoomPage} from '../pages/room/room';
+import {AddRoomPage} from '../pages/add-room/add-room';
+import { FIREBASE_CONFIG } from './firebase.credentials';
+import { RoomProvider } from '../providers/room';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SigninPage,
+    RoomPage,
+    AddRoomPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SigninPage,
+    RoomPage,
+    AddRoomPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    RoomProvider
   ]
 })
 export class AppModule {}
